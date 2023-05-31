f8423189a771c503f70821ba2ee921304acf9218
Vsevolod Mukhin <sevamukh@gmail.com>
2023-05-31 11:29:20 +0700

    second test for squreAllElements() added

--- src/test/kotlin/ru/tinkoff/fintech/SquareAllElementsTest.kt
+++ src/test/kotlin/ru/tinkoff/fintech/SquareAllElementsTest.kt
@@ -6,10 +6,18 @@ import org.junit.jupiter.api.Test
 class SquareAllElementsTest {
 
     @Test
-    fun squareAllElementsShouldReturnValidResult() {
+    fun squareAllElementsShouldReturnValidResultForNonemptyList() {
         val actualList = arrayListOf(-1, 0, 2)
         actualList.squareAllElements()
         assertEquals(arrayListOf(1, 0, 4), actualList,
             "Допущена ошибка при возведении элементов коллекции в квадрат")
+    }
+
+    @Test
+    fun squareAllElementsShouldReturnValidResultForEmptyList() {
+        val actualList = arrayListOf<Int>()
+        actualList.squareAllElements()
+        assertEquals(arrayListOf<Int>(), actualList,
+            "Допущена ошибка для пустого списка")
     }
 }
